58ea2e7e19f0a3e69e280cb236bb412547903b88
UbunGit <296019487@qq.com>
2020-03-09 19:52:16 +0800

    彩票

---
+++ code/test/TensorFlow01.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+from tensorflow.keras.layers import Dense, Flatten, Conv2D
+from tensorflow.keras import Model
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+print(x_train, x_test)
